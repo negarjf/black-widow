c03a2d3ea4cbcfa85eac8fc2ba4a9b5d653222d0
Negar <n.jamalifard@gmail.com>
2021-10-16 01:52:56 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Under Construstion</h1>
+      <h2>
+        <i>Be back soon...</i>
+      </h2>
+    </div>
+  );
+}
