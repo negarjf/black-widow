8b92a2b95071ab6fa1f159b46dc1c6f4095a82ed
Negar <n.jamalifard@gmail.com>
2022-06-28 15:16:08 +0200

    Update landing page (#1)
    Update landing page

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import {
+  Box,
+  Container,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Link,
+  Text,
+  VStack
+} from "@chakra-ui/react";
+import { Header } from "./components/Header";
+import { SocialLinks } from "./components/SocialLinks";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <Container maxW="1800">
+      <Grid
+        width="100%"
+        height={["100%", "100vh"]}
+        templateRows="100px auto"
+        pb="5"
+      >
+        <Header />
+        <Grid
+          as="main"
+          height="100%"
+          templateColumns={["1fr", "1fr", "repeat(2, 1fr)", "repeat(2, 1fr)"]}
+          templateRows="repeat(4, 1fr)"
+          gap={["5", "5", "5", "10"]}
+        >
+          <GridItem
+            rowSpan={[2, 2, 3]}
+            colSpan={[2, 2, 1]}
+            order={[2, 2, 1]}
+            as={Flex}
+            justifyContent="center"
+            flexDirection="column"
+            p="5"
+          >
+            <VStack maxW="600" alignItems="start">
+              <Heading size="2xl" mb="2">
+                hi there,
+              </Heading>
+              <Text size="lg">
+                This is <b>Negar</b>. I am a <b>fontend developer</b> based in{" "}
+                <b>Berlin</b> and originally from <b>Iran</b>. I have studied
+                physics with the dream of becoming an astrophysicist, before I
+                start self-sudying web development. You can read the full story{" "}
+                <Link
+                  href="https://dev.to/negarjf/to-all-of-my-talented-sisters-28jk"
+                  isExternal
+                  textDecoration="underline"
+                >
+                  here
+                </Link>
+                .
+              </Text>
+
+              <Text size="lg">
+                Throughout my working experience as a web developer I've worked
+                with different tools and frameworks, such as <b>Vue</b> and{" "}
+                <b>React</b>. I am passionate about{" "}
+                <Link
+                  textDecoration="underline"
+                  href="https://blog.negar.dev"
+                  isExternal
+                >
+                  <b>blogging</b>
+                </Link>
+                {" and "}
+                <Link textDecoration="underline" href="#talks" isExternal>
+                  <b>public speaking</b>
+                </Link>
+                .
+              </Text>
+            </VStack>
+          </GridItem>
+          <GridItem
+            bg="gray.500"
+            rowSpan={[1, 2, 3]}
+            colSpan={[2, 2, 1]}
+            order={[1, 1, 2]}
+            boxSize="full"
+            backgroundImage="url('assets/avatar.jpg')"
+            backgroundRepeat="no-repeat"
+            backgroundPosition="center"
+            backgroundSize="cover"
+          />
+          <GridItem
+            as={Flex}
+            colSpan={2}
+            order={3}
+            bg="gray.50"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            pt={4}
+          >
+            <SocialLinks />
+            <Box mt="4" textAlign="center">
+              <Text fontSize="xs" color="gray.500">
+                © Negar Jamalifard | 2022
+              </Text>
+              <Link fontSize="xs" href="https://icons8.com" color="gray.500">
+                Icons by Icons8
+              </Link>
+            </Box>
+          </GridItem>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "./theme";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
+const root = createRoot(rootElement); // createRoot(container!) if you use TypeScript
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
 );
